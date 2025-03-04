4f0c38f35801398add8af5b61d329254c0f42cdc
Kuzminov Vladislav <vladislav_kuzminov.00@mail.ru>
2025-03-04 19:02:33 +0300

    Make header and footer according task

---
+++ include/about_us.php
@@ -0,0 +1,6 @@
+<h3 class="footer-heading mb-4">ABOUT HOMESPASE</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe pariatur reprehenderit vero
+    atque,
+    consequatur id ratione, et non dignissimos culpa? Ut veritatis, quos illum totam quis
+    blanditiis, minima
+    minus odio!</p>
