09caf6e047054cc1bb6ecd6852cedf1866a6e39f
Kuzminov Vladislav <vladislav_kuzminov.00@mail.ru>
2025-03-10 21:27:18 +0300

    Configure cache settings for announcements component on page

--- announcements/index.php
+++ announcements/index.php
@@ -113,7 +113,7 @@ $APPLICATION->SetTitle("Объявления");
 		"BROWSER_TITLE" => "-",
 		"CACHE_FILTER" => "N",
 		"CACHE_GROUPS" => "Y",
-		"CACHE_TIME" => "36000000",
+		"CACHE_TIME" => "86400",
 		"CACHE_TYPE" => "A",
 		"CHECK_DATES" => "Y",
 		"DETAIL_ACTIVE_DATE_FORMAT" => "d.m.Y",
