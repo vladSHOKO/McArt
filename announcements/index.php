224a77149b47699ac1fed9c907ab89c77f303863
Kuzminov Vladislav <vladislav_kuzminov.00@mail.ru>
2025-03-10 21:23:54 +0300

    Include announcements and pagination layout

--- announcements/index.php
+++ announcements/index.php
@@ -1,11 +1,109 @@
 <?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Объявления");
-?><?$APPLICATION->IncludeComponent(
+?>
+    <div class="pt-5">
+        <div class="container">
+            <form class="row">
+
+                <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                    <div class="select-wrap">
+                        <span class="icon icon-arrow_drop_down"></span>
+                        <select name="offer-types" id="offer-types" class="form-control d-block rounded-0">
+                            <option value="">Lot Area</option>
+                            <option value="1000">1000</option>
+                            <option value="800">800</option>
+                            <option value="600">600</option>
+                            <option value="400">400</option>
+                            <option value="200">200</option>
+                            <option value="100">100</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                    <div class="select-wrap">
+                        <span class="icon icon-arrow_drop_down"></span>
+                        <select name="offer-types" id="offer-types" class="form-control d-block rounded-0">
+                            <option value="">Property Status</option>
+                            <option value="For Sale">For Sale</option>
+                            <option value="For Rent">For Rent</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                    <div class="select-wrap">
+                        <span class="icon icon-arrow_drop_down"></span>
+                        <select name="offer-types" id="offer-types" class="form-control d-block rounded-0">
+                            <option value="">Location</option>
+                            <option value="United States">United States</option>
+                            <option value="United Kingdom">United Kingdom</option>
+                            <option value="Canada">Canada</option>
+                            <option value="Belgium">Belgium</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                    <div class="select-wrap">
+                        <span class="icon icon-arrow_drop_down"></span>
+                        <select name="offer-types" id="offer-types" class="form-control d-block rounded-0">
+                            <option value="">Lot Area</option>
+                            <option value="1000">1000</option>
+                            <option value="800">800</option>
+                            <option value="600">600</option>
+                            <option value="400">400</option>
+                            <option value="200">200</option>
+                            <option value="100">100</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                    <div class="select-wrap">
+                        <span class="icon icon-arrow_drop_down"></span>
+                        <select name="offer-types" id="offer-types" class="form-control d-block rounded-0">
+                            <option value="">Bedrooms</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5+">5+</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                    <div class="select-wrap">
+                        <span class="icon icon-arrow_drop_down"></span>
+                        <select name="offer-types" id="offer-types" class="form-control d-block rounded-0">
+                            <option value="">Bathrooms</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5+">5+</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                    <div class="mb-4">
+                        <div id="slider-range" class="border-primary"></div>
+                        <input type="text" name="text" id="amount" class="form-control border-0 pl-0 bg-white"
+                               disabled=""/>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                    <input type="submit" class="btn btn-primary btn-block form-control-same-height rounded-0"
+                           value="Search">
+                </div>
+
+            </form>
+
+
+        </div>
+    </div>
+<? $APPLICATION->IncludeComponent(
 	"bitrix:news", 
-	".default", 
+	"custom_announcements_page", 
 	array(
-		"ADD_ELEMENT_CHAIN" => "N",
+		"ADD_ELEMENT_CHAIN" => "Y",
 		"ADD_SECTIONS_CHAIN" => "Y",
 		"AJAX_MODE" => "N",
 		"AJAX_OPTION_ADDITIONAL" => "",
@@ -49,7 +147,7 @@ $APPLICATION->SetTitle("Объявления");
 		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
 		"IBLOCK_ID" => "1",
 		"IBLOCK_TYPE" => "announcement",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
 		"LIST_ACTIVE_DATE_FORMAT" => "d.m.Y",
 		"LIST_FIELD_CODE" => array(
 			0 => "NAME",
@@ -71,13 +169,13 @@ $APPLICATION->SetTitle("Объявления");
 		"MESSAGE_404" => "",
 		"META_DESCRIPTION" => "-",
 		"META_KEYWORDS" => "-",
-		"NEWS_COUNT" => "20",
+		"NEWS_COUNT" => "6",
 		"PAGER_BASE_LINK_ENABLE" => "N",
 		"PAGER_DESC_NUMBERING" => "N",
 		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
 		"PAGER_SHOW_ALL" => "N",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => ".default",
+		"PAGER_SHOW_ALWAYS" => "Y",
+		"PAGER_TEMPLATE" => "custom_pagination",
 		"PAGER_TITLE" => "Объявления",
 		"PREVIEW_TRUNCATE_LEN" => "",
 		"SEF_FOLDER" => "/announcements/",
@@ -98,7 +196,7 @@ $APPLICATION->SetTitle("Объявления");
 		"USE_REVIEW" => "N",
 		"USE_RSS" => "N",
 		"USE_SEARCH" => "N",
-		"COMPONENT_TEMPLATE" => ".default",
+		"COMPONENT_TEMPLATE" => "custom_announcements_page",
 		"DISPLAY_DATE" => "Y",
 		"DISPLAY_PICTURE" => "Y",
 		"DISPLAY_PREVIEW_TEXT" => "Y",
@@ -110,4 +208,11 @@ $APPLICATION->SetTitle("Объявления");
 		)
 	),
 	false
-);?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+); ?>
+
+    </div>
+
+    </div>
+    </div>
+    <br>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
