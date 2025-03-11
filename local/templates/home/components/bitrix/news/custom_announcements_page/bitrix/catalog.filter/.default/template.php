dc883cb26cabdad79c80ad6f75783d6621b0bd38
Kuzminov Vladislav <vladislav_kuzminov.00@mail.ru>
2025-03-11 10:19:53 +0300

    Task task184049

--- local/templates/home/components/bitrix/news/custom_announcements_page/bitrix/catalog.filter/.default/template.php
+++ local/templates/home/components/bitrix/news/custom_announcements_page/bitrix/catalog.filter/.default/template.php
@@ -12,33 +12,100 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<form name="<?echo $arResult["FILTER_NAME"]."_form"?>" action="<?echo $arResult["FORM_ACTION"]?>" method="get">
-	<?foreach($arResult["ITEMS"] as $arItem):
-		if(array_key_exists("HIDDEN", $arItem)):
-			echo $arItem["INPUT"];
-		endif;
-	endforeach;?>
-	<table class="data-table" cellspacing="0" cellpadding="2">
-	<thead>
-		<tr>
-			<td colspan="2" align="center"><?=GetMessage("IBLOCK_FILTER_TITLE")?></td>
-		</tr>
-	</thead>
-	<tbody>
-		<?foreach($arResult["ITEMS"] as $arItem):?>
-			<?if(!array_key_exists("HIDDEN", $arItem)):?>
-				<tr>
-					<td valign="top"><?=$arItem["NAME"]?>:</td>
-					<td valign="top"><?=$arItem["INPUT"]?></td>
-				</tr>
-			<?endif?>
-		<?endforeach;?>
-	</tbody>
-	<tfoot>
-		<tr>
-			<td colspan="2">
-				<input type="submit" name="set_filter" value="<?=GetMessage("IBLOCK_SET_FILTER")?>" /><input type="hidden" name="set_filter" value="Y" />&nbsp;&nbsp;<input type="submit" name="del_filter" value="<?=GetMessage("IBLOCK_DEL_FILTER")?>" /></td>
-		</tr>
-	</tfoot>
-	</table>
-</form>
+<div class="pt-5">
+    <div class="container">
+        <form class="row">
+
+            <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="select-wrap">
+                    <span class="icon icon-arrow_drop_down"></span>
+                    <select name="offer-types" id="offer-types" class="form-control d-block rounded-0">
+                        <option value="">Lot Area</option>
+                        <option value="1000">1000</option>
+                        <option value="800">800</option>
+                        <option value="600">600</option>
+                        <option value="400">400</option>
+                        <option value="200">200</option>
+                        <option value="100">100</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="select-wrap">
+                    <span class="icon icon-arrow_drop_down"></span>
+                    <select name="offer-types" id="offer-types" class="form-control d-block rounded-0">
+                        <option value="">Property Status</option>
+                        <option value="For Sale">For Sale</option>
+                        <option value="For Rent">For Rent</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="select-wrap">
+                    <span class="icon icon-arrow_drop_down"></span>
+                    <select name="offer-types" id="offer-types" class="form-control d-block rounded-0">
+                        <option value="">Location</option>
+                        <option value="United States">United States</option>
+                        <option value="United Kingdom">United Kingdom</option>
+                        <option value="Canada">Canada</option>
+                        <option value="Belgium">Belgium</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="select-wrap">
+                    <span class="icon icon-arrow_drop_down"></span>
+                    <select name="offer-types" id="offer-types" class="form-control d-block rounded-0">
+                        <option value="">Lot Area</option>
+                        <option value="1000">1000</option>
+                        <option value="800">800</option>
+                        <option value="600">600</option>
+                        <option value="400">400</option>
+                        <option value="200">200</option>
+                        <option value="100">100</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="select-wrap">
+                    <span class="icon icon-arrow_drop_down"></span>
+                    <select name="offer-types" id="offer-types" class="form-control d-block rounded-0">
+                        <option value="">Bedrooms</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5+">5+</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="select-wrap">
+                    <span class="icon icon-arrow_drop_down"></span>
+                    <select name="offer-types" id="offer-types" class="form-control d-block rounded-0">
+                        <option value="">Bathrooms</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5+">5+</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="mb-4">
+                    <div id="slider-range" class="border-primary"></div>
+                    <input type="text" name="text" id="amount" class="form-control border-0 pl-0 bg-white"
+                           disabled=""/>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                <input type="submit" class="btn btn-primary btn-block form-control-same-height rounded-0"
+                       value="Search">
+            </div>
+
+        </form>
+
+
+    </div>
+</div>
--- local/templates/home/components/bitrix/news/custom_announcements_page/bitrix/news.detail/.default/template.php
+++ local/templates/home/components/bitrix/news/custom_announcements_page/bitrix/news.detail/.default/template.php
@@ -1,4 +1,4 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 /** @var array $arParams */
 /** @var array $arResult */
 /** @global CMain $APPLICATION */
@@ -12,84 +12,157 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<div class="news-detail">
-	<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arResult["DETAIL_PICTURE"])):?>
-		<img
-			class="detail_picture"
-			border="0"
-			src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>"
-			width="<?=$arResult["DETAIL_PICTURE"]["WIDTH"]?>"
-			height="<?=$arResult["DETAIL_PICTURE"]["HEIGHT"]?>"
-			alt="<?=$arResult["DETAIL_PICTURE"]["ALT"]?>"
-			title="<?=$arResult["DETAIL_PICTURE"]["TITLE"]?>"
-			/>
-	<?endif?>
-	<?if($arParams["DISPLAY_DATE"]!="N" && $arResult["DISPLAY_ACTIVE_FROM"]):?>
-		<span class="news-date-time"><?=$arResult["DISPLAY_ACTIVE_FROM"]?></span>
-	<?endif;?>
-	<?if($arParams["DISPLAY_NAME"]!="N" && $arResult["NAME"]):?>
-		<h3><?=$arResult["NAME"]?></h3>
-	<?endif;?>
-	<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && ($arResult["FIELDS"]["PREVIEW_TEXT"] ?? '')):?>
-		<p><?=$arResult["FIELDS"]["PREVIEW_TEXT"];unset($arResult["FIELDS"]["PREVIEW_TEXT"]);?></p>
-	<?endif;?>
-	<?if($arResult["NAV_RESULT"]):?>
-		<?if($arParams["DISPLAY_TOP_PAGER"]):?><?=$arResult["NAV_STRING"]?><br /><?endif;?>
-		<?echo $arResult["NAV_TEXT"];?>
-		<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?><br /><?=$arResult["NAV_STRING"]?><?endif;?>
-	<?elseif($arResult["DETAIL_TEXT"] <> ''):?>
-		<?echo $arResult["DETAIL_TEXT"];?>
-	<?else:?>
-		<?echo $arResult["PREVIEW_TEXT"];?>
-	<?endif?>
-	<div style="clear:both"></div>
-	<br />
-	<?foreach($arResult["FIELDS"] as $code=>$value):
-		if ('PREVIEW_PICTURE' == $code || 'DETAIL_PICTURE' == $code)
-		{
-			?><?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?
-			if (!empty($value) && is_array($value))
-			{
-				?><img border="0" src="<?=$value["SRC"]?>" width="<?=$value["WIDTH"]?>" height="<?=$value["HEIGHT"]?>"><?
-			}
-		}
-		else
-		{
-			?><?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?><?
-		}
-		?><br />
-	<?endforeach;
-	foreach($arResult["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
+<div class="site-blocks-cover overlay" style="background-image: url(<?= $arResult['DETAIL_PICTURE']['SAFE_SRC'] ?>);"
+     data-aos="fade" data-stellar-background-ratio="0.5">
+    <div class="container">
+        <div class="row align-items-center justify-content-center text-center">
+            <div class="col-md-10">
+                <span class="d-inline-block text-white px-3 mb-3 property-offer-type rounded"><?= GetMessage("DETAILS") ?></span>
+                <h1 class="mb-2"><?= $arResult['PREVIEW_TEXT'] ?></h1>
+                <p class="mb-5"><strong
+                            class="h2 text-success font-weight-bold"><?= $arResult['DISPLAY_PROPERTIES']['PRICE']['VALUE'] . ' ' . GetMessage("RUB") ?></strong>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="site-section site-section-sm">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8" style="margin-top: -150px;">
+                <div class="mb-5">
+                    <div class="slide-one-item home-slider owl-carousel">
+                        <?
+                        $gallery = $arResult['PROPERTIES']['GALLERY_IMAGE']['VALUE'];
+                        if (!empty($gallery)) {
+                            foreach ($gallery as $item) {
+                                $image = CFile::GetFileArray($item);
+                                echo '<div><img src="' . $image['SRC'] . '" alt="Image" class="img-fluid"></div>';
+                            }
+                        }
 
-		<?=$arProperty["NAME"]?>:&nbsp;
-		<?if(is_array($arProperty["DISPLAY_VALUE"])):?>
-			<?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
-		<?else:?>
-			<?=$arProperty["DISPLAY_VALUE"];?>
-		<?endif?>
-		<br />
-	<?endforeach;
-	if(array_key_exists("USE_SHARE", $arParams) && $arParams["USE_SHARE"] == "Y")
-	{
-		?>
-		<div class="news-detail-share">
-			<noindex>
-			<?
-			$APPLICATION->IncludeComponent("bitrix:main.share", "", array(
-					"HANDLERS" => $arParams["SHARE_HANDLERS"],
-					"PAGE_URL" => $arResult["~DETAIL_PAGE_URL"],
-					"PAGE_TITLE" => $arResult["~NAME"],
-					"SHORTEN_URL_LOGIN" => $arParams["SHARE_SHORTEN_URL_LOGIN"],
-					"SHORTEN_URL_KEY" => $arParams["SHARE_SHORTEN_URL_KEY"],
-					"HIDE" => $arParams["SHARE_HIDE"],
-				),
-				$component,
-				array("HIDE_ICONS" => "Y")
-			);
-			?>
-			</noindex>
-		</div>
-		<?
-	}
-	?>
+
+                        ?>
+                    </div>
+                </div>
+                <div class="bg-white">
+                    <div class="row mb-5">
+                        <div class="col-md-6">
+                            <strong class="text-success h1 mb-3"><?= $arResult['DISPLAY_PROPERTIES']['PRICE']['VALUE'] . ' ' . GetMessage("RUB") ?></strong>
+                        </div>
+                        <div class="col-md-6">
+                            <ul class="property-specs-wrap mb-3 mb-lg-0  float-lg-right">
+                                <li>
+                                    <span class="property-specs">Дата обновления</span>
+                                    <span class="property-specs-number"><?= substr($arResult['TIMESTAMP_X'], 0, 10) ?></span>
+
+                                </li>
+                                <li>
+                                    <span class="property-specs">Количество этажей</span>
+                                    <span class="property-specs-number"><?= $arResult['DISPLAY_PROPERTIES']['NUMBER_OF_FLOORS']['VALUE'] ?></span>
+
+                                </li>
+                                <li>
+                                    <span class="property-specs">Площадь</span>
+                                    <span class="property-specs-number"><?= $arResult['DISPLAY_PROPERTIES']['TOTAL_AREA']['VALUE'] ?></span>
+
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="row mb-5">
+                        <div class="col-md-6 col-lg-4 text-left border-bottom border-top py-3">
+                            <span class="d-inline-block text-black mb-0 caption-text">Количество санузлов</span>
+                            <strong class="d-block"><?= $arResult['DISPLAY_PROPERTIES']['NUMBER_OF_BATHROOMS']['VALUE'] ?></strong>
+                        </div>
+                        <div class="col-md-6 col-lg-4 text-left border-bottom border-top py-3">
+                            <span class="d-inline-block text-black mb-0 caption-text">Наличие гаража</span>
+                            <strong class="d-block"><?= $arResult['DISPLAY_PROPERTIES']['GARAGE']['VALUE'] ?></strong>
+                        </div>
+                    </div>
+                    <h2 class="h4 text-black">Детальное описание</h2>
+                    <?= $arResult['DETAIL_TEXT'] ?>
+
+                    <div class="row mt-5">
+                        <div class="col-12">
+                            <h2 class="h4 text-black mb-3">Галерея изображений</h2>
+                        </div>
+                        <?
+                        $gallery = $arResult['PROPERTIES']['GALLERY_IMAGE']['VALUE'];
+                        if (!empty($gallery)) {
+                            foreach ($gallery as $item) {
+                                $image = CFile::GetFileArray($item);
+                                echo '<div class="col-sm-6 col-md-4 col-lg-3 mb-4"><a href="' . $image['SRC'] . '" class="image-popup gal-item"><img src="' . $image['SRC'] . '" alt="Image" class="img-fluid"></a></div>';
+                            }
+                        }
+                        ?>
+
+                    </div>
+                    <?
+                    $additionalFilesID = $arResult['PROPERTIES']['ADDITIONAL_MATERIALS']['VALUE'];
+                    if (!empty($additionalFilesID)):
+                    ?>
+                    <div class="col-12">
+                        <h2 class="h4 text-black mb-3">Дополнительные материалы</h2>
+                    </div>
+                    <div><?
+                        foreach ($additionalFilesID as $fileID) {
+                            $file = CFile::GetFileArray($fileID);
+                            echo '<a href="' . $file['SRC'] . '">' . $file['ORIGINAL_NAME'] . '</a><br>';
+                        }
+                        ?>
+                    </div>
+                    <?endif;?>
+                    <?
+                    $links = $arResult['DISPLAY_PROPERTIES']['LINKS_TO_RESOURCES']['VALUE'];
+                    if (!empty($links)):
+                        ?>
+                        <div class="col-12">
+                            <h2 class="h4 text-black mb-3">Ссылки на внешние ресурсы</h2>
+                        </div>
+                        <div><?
+                            foreach ($links as $link) {
+                                echo '<a href="' . $link . '">' . $link . '</a><br>';
+                            }
+                            ?>
+                        </div>
+                    <?endif;?>
+
+                </div>
+            </div>
+            <div class="col-lg-4 pl-md-5">
+
+                <div class="bg-white widget border rounded">
+
+                    <h3 class="h4 text-black widget-title mb-3">Contact Agent</h3>
+                    <form action="" class="form-contact-agent">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" id="name" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" id="email" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="phone">Phone</label>
+                            <input type="text" id="phone" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" id="phone" class="btn btn-primary" value="Send Message">
+                        </div>
+                    </form>
+                </div>
+
+                <div class="bg-white widget border rounded">
+                    <h3 class="h4 text-black widget-title mb-3">Paragraph</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit qui explicabo, libero nam, saepe
+                        eligendi. Molestias maiores illum error rerum. Exercitationem ullam saepe, minus, reiciendis
+                        ducimus quis. Illo, quisquam, veritatis.</p>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
 </div>
