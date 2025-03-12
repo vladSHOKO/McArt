21320e8617276996d14d1b0098883b2499037a3e
Kuzminov Vladislav <vladislav_kuzminov.00@mail.ru>
2025-03-12 14:32:18 +0300

    Fix mistakes with gitignore

---
+++ bitrix/php_interface/migrations/Version20250312142410.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version20250312142410 extends Version
+{
+    protected $author = "admin";
+
+    protected $description = "";
+
+    protected $moduleVersion = "4.18.2";
+
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+        //your code ...
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}
