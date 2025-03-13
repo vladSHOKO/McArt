8e30092ac7239a8dd7a59befc1d217db09573b50
Kuzminov Vladislav <vladislav_kuzminov.00@mail.ru>
2025-03-13 14:59:36 +0300

    Task 184049 Fix error in rendering announcements with one file

--- local/templates/home/components/bitrix/news/custom_announcements_page/bitrix/news.detail/.default/template.php
+++ local/templates/home/components/bitrix/news/custom_announcements_page/bitrix/news.detail/.default/template.php
@@ -12,13 +12,14 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<div class="site-blocks-cover overlay" style="background-image: url(<?= $arResult['DETAIL_PICTURE']['SAFE_SRC'] ?>);"
+<div class="site-blocks-cover overlay"
+     style="background-image: url(<?= $arResult['DETAIL_PICTURE']['SAFE_SRC'] ?>);"
      data-aos="fade" data-stellar-background-ratio="0.5">
     <div class="container">
         <div class="row align-items-center justify-content-center text-center">
             <div class="col-md-10">
                 <span class="d-inline-block text-white px-3 mb-3 property-offer-type rounded"><?= GetMessage("DETAILS") ?></span>
-                <h1 class="mb-2"><?= $arResult['PREVIEW_TEXT'] ?></h1>
+                <h1 class="mb-2"><?= $arResult['NAME'] ?></h1>
                 <p class="mb-5"><strong
                             class="h2 text-success font-weight-bold"><?= $arResult['DISPLAY_PROPERTIES']['PRICE']['VALUE'] . ' ' . GetMessage("RUB") ?></strong>
                 </p>
@@ -30,101 +31,133 @@ $this->setFrameMode(true);
     <div class="container">
         <div class="row">
             <div class="col-lg-8" style="margin-top: -150px;">
-                <div class="mb-5">
-                    <div class="slide-one-item home-slider owl-carousel">
-                        <?
-                        $gallery = $arResult['DISPLAY_PROPERTIES']['GALLERY_IMAGE']['FILE_VALUE'];
-                        if (!empty($gallery)) {
-                            foreach ($gallery as $image) {
-
-                                echo '<div><img src="' . $image['SRC'] . '" alt="Image" class="img-fluid"></div>';
+                <?
+                $gallery = $arResult['DISPLAY_PROPERTIES']['GALLERY_IMAGE']['FILE_VALUE'];
+                if (!empty($gallery)):
+                    ?>
+                    <div class="mb-5">
+                        <div class="slide-one-item home-slider owl-carousel">
+                            <?
+                            if (key_exists('0', $gallery)) {
+                                foreach ($gallery as $image) {
+                                    echo '<div><img src="' . $image['SRC'] . '" alt="Image" class="img-fluid"></div>';
+                                }
+                            } else {
+                                echo '<div><img src="' . $gallery['SRC'] . '" alt="Image" class="img-fluid"></div>';
                             }
-                        }
 
 
-                        ?>
+                            ?>
+                        </div>
                     </div>
-                </div>
+                <? endif; ?>
                 <div class="bg-white">
                     <div class="row mb-5">
-                        <div class="col-md-6">
-                            <strong class="text-success h1 mb-3"><?= $arResult['DISPLAY_PROPERTIES']['PRICE']['VALUE'] . ' ' . GetMessage("RUB") ?></strong>
-                        </div>
+                        <? if (!empty($arResult['DISPLAY_PROPERTIES']['PRICE']['VALUE'])): ?>
+                            <div class="col-md-6">
+                                <strong class="text-success h1 mb-3"><?= $arResult['DISPLAY_PROPERTIES']['PRICE']['VALUE'] . ' ' . GetMessage("RUB") ?></strong>
+                            </div>
+                        <? endif; ?>
                         <div class="col-md-6">
                             <ul class="property-specs-wrap mb-3 mb-lg-0  float-lg-right">
-                                <li>
-                                    <span class="property-specs">Дата обновления</span>
-                                    <span class="property-specs-number"><?= substr($arResult['TIMESTAMP_X'], 0, 10) ?></span>
+                                <? if (!empty($arResult['TIMESTAMP_X'])): ?>
+                                    <li>
+                                        <span class="property-specs"><?= GetMessage("DATE_OF_UPDATE") ?></span>
+                                        <span class="property-specs-number"><?= substr($arResult['TIMESTAMP_X'], 0, 10) ?></span>
 
-                                </li>
-                                <li>
-                                    <span class="property-specs">Количество этажей</span>
-                                    <span class="property-specs-number"><?= $arResult['DISPLAY_PROPERTIES']['NUMBER_OF_FLOORS']['VALUE'] ?></span>
+                                    </li>
+                                <? endif; ?>
+                                <? if (!empty($arResult['DISPLAY_PROPERTIES']['NUMBER_OF_FLOORS']['VALUE'])): ?>
+                                    <li>
+                                        <span class="property-specs"><?= GetMessage("FLOOR_COUNT") ?></span>
+                                        <span class="property-specs-number"><?= $arResult['DISPLAY_PROPERTIES']['NUMBER_OF_FLOORS']['VALUE'] ?></span>
 
-                                </li>
-                                <li>
-                                    <span class="property-specs">Площадь</span>
-                                    <span class="property-specs-number"><?= $arResult['DISPLAY_PROPERTIES']['TOTAL_AREA']['VALUE'] ?></span>
+                                    </li>
+                                <? endif; ?>
+                                <? if (!empty($arResult['DISPLAY_PROPERTIES']['TOTAL_AREA']['VALUE'])): ?>
+                                    <li>
+                                        <span class="property-specs"><?= GetMessage("TOTAL_AREA") ?></span>
+                                        <span class="property-specs-number"><?= $arResult['DISPLAY_PROPERTIES']['TOTAL_AREA']['VALUE'] ?></span>
 
-                                </li>
+                                    </li>
+                                <? endif; ?>
                             </ul>
                         </div>
                     </div>
                     <div class="row mb-5">
-                        <div class="col-md-6 col-lg-4 text-left border-bottom border-top py-3">
-                            <span class="d-inline-block text-black mb-0 caption-text">Количество санузлов</span>
-                            <strong class="d-block"><?= $arResult['DISPLAY_PROPERTIES']['NUMBER_OF_BATHROOMS']['VALUE'] ?></strong>
-                        </div>
-                        <div class="col-md-6 col-lg-4 text-left border-bottom border-top py-3">
-                            <span class="d-inline-block text-black mb-0 caption-text">Наличие гаража</span>
-                            <strong class="d-block"><?= $arResult['DISPLAY_PROPERTIES']['GARAGE']['VALUE'] ?></strong>
-                        </div>
+                        <? if (!empty($arResult['DISPLAY_PROPERTIES']['NUMBER_OF_BATHROOMS']['VALUE'])): ?>
+                            <div class="col-md-6 col-lg-4 text-left border-bottom border-top py-3">
+                                <span class="d-inline-block text-black mb-0 caption-text"><?= GetMessage("NUMBER_OF_BATHROOMS") ?></span>
+                                <strong class="d-block"><?= $arResult['DISPLAY_PROPERTIES']['NUMBER_OF_BATHROOMS']['VALUE'] ?></strong>
+                            </div>
+                        <? endif; ?>
+
+                        <? if (!empty($arResult['DISPLAY_PROPERTIES']['GARAGE']['VALUE'])): ?>
+                            <div class="col-md-6 col-lg-4 text-left border-bottom border-top py-3">
+                                <span class="d-inline-block text-black mb-0 caption-text"><?= GetMessage("AVAILABILITY_OF_GARAGE") ?></span>
+                                <strong class="d-block"><?= $arResult['DISPLAY_PROPERTIES']['GARAGE']['VALUE'] ?></strong>
+                            </div>
+                        <? endif; ?>
                     </div>
-                    <h2 class="h4 text-black">Детальное описание</h2>
+                    <h2 class="h4 text-black"><?= GetMessage("DETAIL_DESCRIPTION") ?></h2>
                     <?= $arResult['DETAIL_TEXT'] ?>
 
-                    <div class="row mt-5">
-                        <div class="col-12">
-                            <h2 class="h4 text-black mb-3">Галерея изображений</h2>
-                        </div>
-                        <?
-                        $gallery = $arResult['DISPLAY_PROPERTIES']['GALLERY_IMAGE']['FILE_VALUE'];
-                        if (!empty($gallery)) {
-                            foreach ($gallery as $image) {
-                                echo '<div class="col-sm-6 col-md-4 col-lg-3 mb-4"><a href="' . $image['SRC'] . '" class="image-popup gal-item"><img src="' . $image['SRC'] . '" alt="Image" class="img-fluid"></a></div>';
+                    <? if (!empty($gallery)): ?>
+                        <div class="row mt-5">
+                            <div class="col-12">
+                                <h2 class="h4 text-black mb-3"><?= GetMessage("IMAGE_GALLERY") ?></h2>
+                            </div>
+                            <?
+                            if (key_exists('0', $gallery)) {
+                                foreach ($gallery as $image) {
+                                    echo '<div class="col-sm-6 col-md-4 col-lg-3 mb-4"><a href="' . $image['SRC'] . '" class="image-popup gal-item"><img src="' . $image['SRC'] . '" alt="Image" class="img-fluid"></a></div>';
+                                }
+                            } else {
+                                echo '<div class="col-sm-6 col-md-4 col-lg-3 mb-4"><a href="' . $gallery['SRC'] . '" class="image-popup gal-item"><img src="' . $gallery['SRC'] . '" alt="Image" class="img-fluid"></a></div>';
                             }
-                        }
-                        ?>
 
-                    </div>
+                            ?>
+
+                        </div>
+                    <? endif; ?>
                     <?
                     $additionalFiles = $arResult['DISPLAY_PROPERTIES']['ADDITIONAL_MATERIALS']['FILE_VALUE'];
                     if (!empty($additionalFiles)):
-                    ?>
-                    <div class="col-12">
-                        <h2 class="h4 text-black mb-3">Дополнительные материалы</h2>
-                    </div>
-                    <div><?
-                        foreach ($additionalFiles as $file) {
-                            echo '<a href="' . $file['SRC'] . '">' . $file['ORIGINAL_NAME'] . '</a><br>';
-                        }
                         ?>
-                    </div>
-                    <?endif;?>
-                    <?
-                    $links = $arResult['DISPLAY_PROPERTIES']['LINKS_TO_RESOURCES']['VALUE'];
-                    if (!empty($links)):
-                        ?>
+
                         <div class="col-12">
-                            <h2 class="h4 text-black mb-3">Ссылки на внешние ресурсы</h2>
+                            <h2 class="h4 text-black mb-3"><?= GetMessage("ADDITIONAL_FILES") ?></h2>
                         </div>
                         <div><?
-                            foreach ($links as $link) {
-                                echo '<a href="' . $link . '">' . $link . '</a><br>';
+                            if (key_exists('0', $additionalFiles)) {
+                                foreach ($additionalFiles as $file) {
+                                    echo '<a href="' . $file['SRC'] . '">' . $file['ORIGINAL_NAME'] . '</a><br>';
+                                }
+                            } else {
+                                echo '<a href="' . $additionalFiles['SRC'] . '">' . $additionalFiles['ORIGINAL_NAME'] . '</a><br>';
                             }
+
                             ?>
                         </div>
-                    <?endif;?>
+                    <? endif; ?>
+                    <?
+                    $links = $arResult['DISPLAY_PROPERTIES']['LINKS_TO_RESOURCES']['VALUE'];
+                    if (!empty($links)):?>
+                        <div class="col-12">
+                            <h2 class="h4 text-black mb-3"><?= GetMessage("FOREIGN_LINKS") ?></h2>
+                        </div>
+                        <div><?
+                            if (key_exists('0', $links)) {
+                                foreach ($links as $link) {
+                                    echo '<a href="' . $link . '">' . $link . '</a><br>';
+                                }
+                            } else {
+                                echo '<a href="' . $links . '">' . $links . '</a><br>';
+                            }
+
+                            ?>
+                        </div>
+                    <? endif; ?>
 
                 </div>
             </div>
