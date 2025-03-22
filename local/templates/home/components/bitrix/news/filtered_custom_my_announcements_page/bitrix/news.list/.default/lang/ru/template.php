8c12566bbd99188d5da4468c41f5375127293a42
Kuzminov Vladislav <vladislav_kuzminov.00@mail.ru>
2025-03-22 12:23:53 +0300

    Delete unnecessary news template

--- local/templates/home/components/bitrix/news/filtered_custom_my_announcements_page/bitrix/news.list/.default/lang/ru/template.php
+++
@@ -1,7 +0,0 @@
-<?
-$MESS["CT_BNL_ELEMENT_DELETE_CONFIRM"] = "Будет удалена вся информация, связанная с этой записью. Продолжить?";
-$MESS["AREA"] = "Площадь";
-$MESS["FLOOR"] = "Этажей";
-$MESS["BATHROOM"] = "Санузлов";
-$MESS["GARAGE"] = "Гараж";
-?>
