cde0b9f5a73600c0bab67383e8c8c74151be2291
Kuzminov Vladislav <vladislav_kuzminov.00@mail.ru>
2025-03-25 13:38:32 +0300

    Add event handler for agent changes

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,5 +1,12 @@
 <?php
 
 require_once "include/OnAfterUserRegisterHandler.php";
+require_once "include/HiloadBlocksHandler.php";
 
 AddEventHandler("main", "OnAfterUserRegister", ["OnAfterUserRegisterHandler", "defineUserGroup"]);
+
+$eventManager = \Bitrix\Main\EventManager::getInstance();
+
+$eventManager->addEventHandler("", "AgentsOnAfterAdd", "HiloadBlocksHandler::updateCache");
+$eventManager->addEventHandler("", "AgentsOnAfterUpdate", "HiloadBlocksHandler::updateCache");
+$eventManager->addEventHandler("", "AgentsOnAfterDelete", "HiloadBlocksHandler::updateCache");
